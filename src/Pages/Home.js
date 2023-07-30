16eacbcfb3b186f264b4824a7e431ff3ac55932b
Pravesh <pritamyadav70923@gmail.com>
2023-07-30 14:34:00 +0530

    api calls in react

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,18 @@
+import React from "react";
 import classes from "./Home.module.css"
 const Home=()=>{
+  const fetchMovies=async()=>{
+
+    const response = await fetch('https://swapi.dev/api/films/');
+    const responseData= await response.json();
+    console.log(responseData.results);
+    
+
+}
     return(
         <div className={classes.outer}>
         <div >
-          <button className={classes.btn}>Get our Latest Album</button>
+          <button onClick={fetchMovies} className={classes.btn}>Get our Latest Album</button>
         </div>
         <div className={classes.playBtn}>
         <div className={classes['arrow-right']}></div> </div>
