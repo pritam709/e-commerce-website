b552566127d01565cbc4634fe0e51405c4453117
Pravesh <pritamyadav70923@gmail.com>
2023-08-01 20:44:10 +0530

    useEffect implement

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,24 +1,58 @@
-import React from "react";
-import classes from "./Home.module.css"
-const Home=()=>{
-  const fetchMovies=async()=>{
+import React, { useState,useEffect } from "react";
+import classes from "./Home.module.css";
+const Home = () => {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const response = await fetch('https://swapi.dev/api/films/');
-    const responseData= await response.json();
-    console.log(responseData.results);
-    
+  useEffect(()=>{
+    const fetchMovies = async () => {
+      setIsLoading(true);
+      setError(null);
+  
+      try {
+        const response = await fetch("https://swapi.dev/api/films/");
+        if (!response.ok) {
+          throw new Error("something went wrong");
+        }
+        const responseData = await response.json();
+  
+        console.log(responseData.results);
+        setMovies(responseData.results);
+        
+      } catch (error) {
+        setError(error.message)
+      }
+  
+      setIsLoading(false);
+    };
 
-}
-    return(
-        <div className={classes.outer}>
-        <div >
-          <button onClick={fetchMovies} className={classes.btn}>Get our Latest Album</button>
+    fetchMovies();
+  },[])
+
+ 
+  return (
+    <>
+      <div className={classes.outer}>
+        <div>
+          <button className={classes.btn}>
+            Get our Latest Album
+          </button>
         </div>
         <div className={classes.playBtn}>
-        <div className={classes['arrow-right']}></div> </div>
-
+          <div className={classes["arrow-right"]}></div>{" "}
         </div>
-    )
-
-}
+      </div>
+      <div>
+        {isLoading && <p>Loading......</p>}
+        {!isLoading && error && <p> {error}</p>}
+        <ul>
+          {movies.map((item) => {
+            return <li key={item.episode_id}>{item.title}</li>;
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
 export default Home;
